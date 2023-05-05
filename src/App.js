7b8e3a53b7e3172c31052f0c91a8f56f2a8c32e9
kdh <ajdshdl@naver.com>
2023-05-05 23:07:52 +0900

    20230505 array, object state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-//import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  let post = '강남 우동 맛집';
   let [글제목, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
-  
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
+        
       </div>
         <div className='list'>
-        <h4>{ 글제목[0] }</h4>
+        <h4>{ 글제목[0] } <span onClick={()=> {따봉변경(따봉 + 1)}}> 👍 </span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
@@ -25,6 +24,10 @@ function App() {
         <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <button onClick={()=>{
+        b(['여자코트 추천', '강남 우동맛집', '파이썬독학']);
+        }}>글 수정</button>
     </div>
   );
 }
