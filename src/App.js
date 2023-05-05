0eee1436a35e1db9c7abe238f7da6bb3702910f9
kdh <ajdshdl@naver.com>
2023-05-05 22:33:38 +0900

    20230505 edit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import './App.css';
 function App() {
 
   let post = '강남 우동 맛집';
+  let [글제목, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
   
-  let [글제목1, a] = useState('남자코트 추천');
-  let [글제목2, b] = useState('강남 우동맛집');
-  let [글제목3, c] = useState('파이썬독학');
 
   return (
     <div className="App">
@@ -16,15 +14,15 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
         <div className='list'>
-        <h4>{ 글제목1 }</h4>
+        <h4>{ 글제목[0] }</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{ 글제목2 }</h4>
+        <h4>{ 글제목[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{ 글제목3 }</h4>
+        <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
